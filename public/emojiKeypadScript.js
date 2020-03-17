c191adfb0c8e99ad87f4683a41251900e92034fd
ofsouzap <ofsouzap@gmail.com>
2020-03-17 21:26:31 +0000

    In with the new
    Added the recent emojis panel on the emoji "dropup" menu
    The menu holds {emoji table column count} * {a constant (currently 2)} emojis
    The menu won't hold duplicates
    The menu works by storing the indexes of the emojis from the "emojis" array

--- public/emojiKeypadScript.js
+++ public/emojiKeypadScript.js
@@ -4,7 +4,17 @@ let paleEmojis = "👶🏻 👦🏻 👧🏻 👨🏻 👩🏻 👱🏻‍♀️
 emojiString = emojiString.concat(paleEmojis);
 var emojis = emojiString.split(" ");
 let emojiKeypadEnabled = false;
+
 var emojiDropup;
+var recentEmojisTable;
+
+let columnCount = 6;
+
+let recentEmojiRows = 2;
+/*Length of recent emojis should always be the same as columnCount multiplied by recentEmojiRows*/
+var recentEmojiIndexes = [];
+
+function getRecentEmojiIndexesMaxLength() { return columnCount * recentEmojiRows; }
 
 function setEmojiKeypadState(state) {
 	
@@ -28,43 +38,109 @@ function refreshEmojiKeypadState() {
 function onEmojiSelected(emoji) {
 	
 	document.getElementById("m").value += emoji;
+	tryAddRecentEmoji(emoji);
+	
+}
+
+function getEmojiIndex(emoji) {
+	
+	for (var i = 0; i < emojis.length; i++) {
+		
+		if (emojis[i] == emoji) {
+			return i;
+		}
+		
+	}
+	
+}
+
+function tryAddRecentEmoji(emoji) {
+	
+	let emojiIndex = getEmojiIndex(emoji);
+	
+	if (recentEmojiIndexes.includes(emojiIndex)) { return; }
+	
+	recentEmojiIndexes.unshift(emojiIndex);
+	
+	if (recentEmojiIndexes.length > getRecentEmojiIndexesMaxLength()) {
+		
+		recentEmojiIndexes.pop();
+		
+	}
+	
+	refreshRecentEmojis();
+	
+}
+
+function refreshRecentEmojis() {
+	
+	recentEmojisTable.innerHTML = "";
+	
+	for (var row = 0; row < recentEmojiRows; row++) {
+	
+		var tableRow = document.createElement("tr");
+		
+		for (var column = 0; column < columnCount; column++) {
+			
+			var emoji = emojis[recentEmojiIndexes[(row * columnCount) + column]];
+			if (emoji == null) { break; }
+			
+			var tableData = createEmojiButton(emoji);
+			
+			tableRow.appendChild(tableData);
+			
+		}
+		
+		recentEmojisTable.appendChild(tableRow);
+	
+	}
 	
 }
 
 window.onload = function() {
 	
 	emojiDropup = document.getElementById("emojiDropup");
+	recentEmojisTable = document.getElementById("recentEmojisTable");
 	
-	let columns = 6;
-	var rows = emojis.length / columns;
+	var emojiDropupTable = document.getElementById("emojiDropupTable");
 	
-	if (rows * columns < emojis.length) { rows++; }
+	var rows = emojis.length / columnCount;
+	
+	if (rows * columnCount < emojis.length) { rows++; }
 	
 	for (var row = 0; row < rows; row++) {
 		
 		var tableRow = document.createElement("tr");
 		
-		for (var column = 0; column < columns; column++) {
+		for (var column = 0; column < columnCount; column++) {
 			
-			var tableData = document.createElement("td");
-			tableData.className = "emojiKeypadOption";
-			
-			var emoji = emojis[(row * columns) + column];
+			var emoji = emojis[(row * columnCount) + column];
 			if (emoji == null) { break; }
 			
-			tableData.innerText = emoji;
-			
-			tableData.setAttribute("onclick", "onEmojiSelected('" + emoji + "')");
+			var tableData = createEmojiButton(emoji);
 			
 			tableRow.appendChild(tableData);
 			
 		}
 		
-		emojiDropup.appendChild(tableRow);
+		emojiDropupTable.appendChild(tableRow);
 		
 	}
 	
 	refreshEmojiKeypadState();
+	
+}
+
+function createEmojiButton(emoji) {
+	
+	var tableData = document.createElement("td");
+	tableData.className = "emojiKeypadOption";
+	
+	tableData.innerText = emoji;
+	
+	tableData.setAttribute("onclick", "onEmojiSelected('" + emoji + "')");
+	
+	return tableData;
 	
 }
 
